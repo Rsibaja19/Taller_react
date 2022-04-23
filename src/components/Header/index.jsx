31548114ec72ad696078a035cea4eca6cbcf7c54
Ricardo Sibaja <rkjsc2000@gmail.com>
2022-04-23 18:34:42 -0500

    feat: header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1 className="font-black text-4xl md:w-2/3 text-blue-900">Clientes</h1>
+      <p className="mb-2 text-2xl">Administra tus clientes</p>
+    </div>
+  );
+};
+
+export default index;
