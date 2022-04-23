5757eb13946e8cca8d562718908d5fe9562737c3
Ricardo Sibaja <rkjsc2000@gmail.com>
2022-04-23 18:33:50 -0500

    feat: update alert

--- src/components/Alerta/index.jsx
+++ src/components/Alerta/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const index = ({ children }) => {
   return (
-    <div className="text-center my-4 bg-red-600 text-white font-bold p-3 uppercase">
+    <div className="text-center my-2 bg-red-600 text-white font-bold p-2 uppercase">
       {children}
     </div>
   );
