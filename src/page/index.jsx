f284d816d12e79e5e0eb90a8815acf7dd1d45046
Ricardo Sibaja <rkjsc2000@gmail.com>
2022-04-23 18:35:39 -0500

    feat: page

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Formulario from "../components/FormCliente";
+import Clientes from "../components/Clientes";
 
 const index = () => {
+  const [clientes, setClientes] = useState([]);
+  const [editCliente, setEditCliente] = useState({});
   return (
-    <div className="container-1440">
-      <h1>Good Practics</h1>
+    <div className="container-1440 mx-auto mt-5">
+      <Header />
+      <div className="mt-5 xl:flex justify-center pb-4 scroll">
+        <Formulario
+          clientes={clientes}
+          setClientes={setClientes}
+          editCliente={editCliente}
+          setEditCliente={setEditCliente}
+        />
+        <Clientes
+          clientes={clientes}
+          setClientes={setClientes}
+          setEditCliente={setEditCliente}
+        />
+      </div>
     </div>
   );
 };
