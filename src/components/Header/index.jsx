f9de12e6786904ea39b714b648be202f904cb225
Ricardo Sibaja <rkjsc2000@gmail.com>
2022-04-23 19:19:28 -0500

    feat: update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const index = () => {
   return (
     <div>
-      <h1 className="font-black text-4xl md:w-2/3 text-blue-900">Clientes</h1>
-      <p className="mb-2 text-2xl">Administra tus clientes</p>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto mb-10">
+        Seguimiento {""}
+        <span className="text-indigo-600">Clientes</span>
+      </h1>
     </div>
   );
 };
